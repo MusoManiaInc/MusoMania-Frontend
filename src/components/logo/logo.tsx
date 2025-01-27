7ffcfca8f5fba36aec3bad2ead83cd9a350686a1
ksdao <kalemsdao03@gmail.com>
2025-01-26 19:06:09 -0500

    BTS-18 <Logo Dynamic>

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+const LogoComponent = () => {
+  const [currentImage, setCurrentImage] = useState<string>("");
+
+  useEffect(() => {
+    const images: { [key in "Sunday" | "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday"]: string } = {
+      Sunday: "/icons/logoSu.png",
+      Monday: "/icons/logoM.png",
+      Tuesday: "/icons/logoTu.png",
+      Wednesday: "/icons/logoW.png",
+      Thursday: "/icons/logoTh.png",
+      Friday: "/icons/logoF.png",
+      Saturday: "/icons/logoSa.png",
+    };
+
+    const today = new Date().toLocaleString("en-US", { weekday: "long" }) as keyof typeof images;
+    setCurrentImage(images[today]);
+  }, []);
+
+  return (
+      <img className="h-16" src={currentImage} alt="MusoMania footer logo" />
+  );
+};
+
+export default LogoComponent
