9c4e563cb09ac5fc56f874ce69350548603554bb
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-01-28 13:40:03 -0500

    added careers page route

---
+++ src/app/careers/page.tsx
@@ -0,0 +1,12 @@
+import HomeFooter from "@/components/footer/home-footer";
+import HomeHeader from "@/components/navbar/home-header";
+
+export default function Careers() {
+  return (
+    <div className="min-h-screen flex flex-col">
+        <HomeHeader />
+            <div>Welcome to the careers page!</div>
+        <HomeFooter />
+    </div>
+  )
+}
