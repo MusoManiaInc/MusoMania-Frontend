7f3129d1978c4632ca789158e7c0409c49d939f1
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-02-03 16:43:59 -0500

    BTS-15 masthead complete

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -11,7 +11,7 @@ export default function Careers() {
           <div className="px-6 py-8 flex flex-col gap-10 justify-center items-center w-full bg-yellow-50">
             <div>
               <h1 className="text-5xl text-center font-semibold pt-20">Make a lasting impact<br />on the future of music</h1>
-            </div>
+            </div> 
             <div>
               <a className="bg-custom-purple2 text-white px-6 py-3 rounded-full hover:bg-custom-purple1 transition-all font-bold z-10" href="/#open-roles">See open roles</a>
             </div>
