0636f333bafa070420d6d008bdc7f719a47fcfbd
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-02-03 17:00:00 -0500

    BTS-15 info section done

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -26,7 +26,7 @@ export default function Careers() {
                 <h1 className="text-3xl text-center font-semibold">MusoMania in a nutshell.</h1>
                 <p className="pt-2">We help shape culture and society.</p>
               </div>
-              <div>
+              <div> 
                 <p>MusoMania was founded in 2025 as a social media platform designed for musicians to <b>connect, collaborate, and monetize</b> their talents. Our platform <b>empowers musicians</b> by allowing them to share their music, find bandmates, sell rights or sheet music, and even offer their skills as a service (e.g. recording drum loops).</p>
                 <p className="pt-2">You can come work with us in cities across Canada.</p>
               </div>
