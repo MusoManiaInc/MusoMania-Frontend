cbff068f4bf2081f585c20a646b3c51f04fd5c96
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-02-03 17:14:05 -0500

    fixed careers button link

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -13,7 +13,7 @@ export default function Careers() {
               <h1 className="text-5xl text-center font-semibold pt-20">Make a lasting impact<br />on the future of music</h1>
             </div> 
             <div>
-              <a className="bg-custom-purple2 text-white px-6 py-3 rounded-full hover:bg-custom-purple1 transition-all font-bold z-10" href="/#open-roles">See open roles</a>
+              <a className="bg-custom-purple2 text-white px-6 py-3 rounded-full hover:bg-custom-purple1 transition-all font-bold z-10" href="#open-roles">See open roles</a>
             </div>
             <div>
               <img className="h-96 z-0 -mt-10" src="/images/careers-page.svg" alt="A diverse group of business men and women graphic" />
