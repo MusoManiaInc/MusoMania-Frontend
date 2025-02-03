ef5a8c1f6cb66a7538ce604f758528f154d0afc0
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-02-03 16:56:51 -0500

    BTS-16 completed info section of careers page

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -19,6 +19,21 @@ export default function Careers() {
               <img className="h-96 z-0 -mt-10" src="/images/careers-page.svg" alt="A diverse group of business men and women graphic" />
             </div>
           </div>
+          {/* Careers page info section */}
+          <div className="px-6 py-8 flex flex-col gap-10 justify-center items-center">
+            <div className="px-6 py-8 flex flex-col gap-10 justify-center items-center text-center lg:mx-44">
+              <div>
+                <h1 className="text-3xl text-center font-semibold">MusoMania in a nutshell.</h1>
+                <p className="pt-2">We help shape culture and society.</p>
+              </div>
+              <div>
+                <p>MusoMania was founded in 2025 as a social media platform designed for musicians to <b>connect, collaborate, and monetize</b> their talents. Our platform <b>empowers musicians</b> by allowing them to share their music, find bandmates, sell rights or sheet music, and even offer their skills as a service (e.g. recording drum loops).</p>
+              </div>
+              <div>
+                <a href="/about" className="px-6 py-3 rounded-full hover:bg-gray-100 transition-all border border-black">Learn more</a>
+              </div>
+            </div>
+          </div>
         </div>
         <HomeFooter />
     </div>
