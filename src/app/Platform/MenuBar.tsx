2d1a977ba1c56122296876e46c1b5d31219084bf
miran <miranararat722@gmail.com>
2025-02-06 15:25:41 -0500

    change  up menu bar

--- src/app/Platform/MenuBar.tsx
+++ src/app/Platform/MenuBar.tsx
@@ -1,21 +1,12 @@
-import { validateRequest } from "@/auth";
 import { Button } from "@/components/ui/button";
-import prisma from "@/lib/prisma";
-import { Bookmark, Home } from "lucide-react";
+import { Bell, Bookmark, Home, Mail } from "lucide-react";
 import Link from "next/link";
-
 
 interface MenuBarProps {
     className?: string;
 }
 
-export default async function MenuBar({ className }: MenuBarProps) {
-    const { user } = await validateRequest();
-
-    if (!user) return null;
-
-    
-
+export default function MenuBar({ className }: MenuBarProps) {
     return (
         <div className={className}>
             <Button
@@ -29,7 +20,28 @@ export default async function MenuBar({ className }: MenuBarProps) {
                     <span className="hidden lg:inline">Home</span>
                 </Link>
             </Button>
-            
+            <Button
+                variant="ghost"
+                className="flex items-center justify-start gap-3"
+                title="Notifications"
+                asChild
+            >
+                <Link href="/notifications">
+                    <Bell />
+                    <span className="hidden lg:inline">Notifications</span>
+                </Link>
+            </Button>
+            <Button
+                variant="ghost"
+                className="flex items-center justify-start gap-3"
+                title="Messages"
+                asChild
+            >
+                <Link href="/messages">
+                    <Mail />
+                    <span className="hidden lg:inline">Messages</span>
+                </Link>
+            </Button>
             <Button
                 variant="ghost"
                 className="flex items-center justify-start gap-3"
