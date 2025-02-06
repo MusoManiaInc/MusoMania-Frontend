f42b848a565248be78a68add2093e3fdc7afc6e2
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2025-02-06 13:16:29 -0500

    BTS-13 Added faq page route

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,12 @@
+import HomeFooter from "@/components/footer/home-footer";
+import HomeHeader from "@/components/navbar/home-header";
+
+export default function FAQ() {
+    return (
+        <div className="min-h-screen flex flex-col w-full">
+            <HomeHeader />
+            <div>FAQ Page</div>
+            <HomeFooter />
+        </div>
+    )
+}
