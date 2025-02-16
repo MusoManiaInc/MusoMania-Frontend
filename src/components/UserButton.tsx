6876d6b0041b263e5c5261d1f7573efbdd2266bf
miran <miranararat722@gmail.com>
2025-02-16 12:39:17 -0500

    Add user profile

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -42,7 +42,7 @@ export default function UserButton({ className }: UserButtonProps) {
             <DropdownMenuContent>
                 <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <Link href={`/users/${user.username}`}>
+                <Link href={`feed/users/${user.username}`}>
                     <DropdownMenuItem>
                         <UserIcon className="mr-2 size-4" />
                         Profile
