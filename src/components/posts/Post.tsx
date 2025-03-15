f71d22e98eadc0509ea786d4fe6a6d40a4c5a3e4
miran <miranararat722@gmail.com>
2025-03-15 16:25:19 -0400

    add comments

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -11,12 +11,16 @@ import PostMoreButton from "./PostMoreButton";
 import PostReportButton from "./PostReportButton";
 import LikeButton from "./LikeButton";
 import BookmarkButton from "./BookmarkButton";
+import { MessageSquare } from "lucide-react";
+import { useState } from "react";
+import Comments from "../comments/Comments";
 interface PostProps {
   post: PostData;
 }
 
 export default function Post({ post }: PostProps) {
   const { user } = useSession();
+  const [showComments, setShowComments] = useState(false);
 
   return (
     <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
@@ -52,13 +56,19 @@ export default function Post({ post }: PostProps) {
       {!!post.attachments.length && (
         <MediaPreviews attachments={post.attachments} />
       )}
-      <LikeButton
-        postId={post.id}
-        initialState={{
-          likes: post._count.likes,
-          isLikedByUser: post.likes.some((like) => like.userId === user.id),
-        }}
-      />
+      <div className="flex items-center gap-5">
+        <LikeButton
+          postId={post.id}
+          initialState={{
+            likes: post._count.likes,
+            isLikedByUser: post.likes.some((like) => like.userId === user.id),
+          }}
+        />
+        <CommentButton
+          post={post}
+          onClick={() => setShowComments(!showComments)}
+        />
+      </div>
       <BookmarkButton
         postId={post.id}
         initialState={{
@@ -74,6 +84,7 @@ export default function Post({ post }: PostProps) {
       />
       )}
       </div>
+      {showComments && <Comments post={post} />}
     </article>
   );
 }
@@ -127,4 +138,19 @@ function MediaPreview({ media }: MediaPreviewProps) {
   }
 
   return <p className="text-destructive">Unsupported media type</p>;
+}
+interface CommentButtonProps {
+  post: PostData;
+  onClick: () => void;
+}
+function CommentButton({ post, onClick }: CommentButtonProps) {
+  return (
+    <button onClick={onClick} className="flex items-center gap-2">
+      <MessageSquare className="size-5" />
+      <span className="text-sm font-medium tabular-nums">
+        {post._count.comments}{" "}
+        <span className="hidden sm:inline">comments</span>
+      </span>
+    </button>
+  );
 }
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,4 +1,3 @@
-import exp from "constants";
 import { z } from "zod";
 
 const requiredString = z.string().trim().min(1, "Required");
@@ -23,16 +22,16 @@ export type LoginValues = z.infer<typeof loginSchema>;
 
 export const createPostSchema = z.object({
     content: requiredString,
-    mediaIds: z.array(z.string()).max(5, "Cannot have more than 5 attachments")
+    mediaIds: z.array(z.string()).max(5, "Cannot have more than 5 attachments"),
 });
 
 export const updateUserProfileSchema = z.object({
     displayName: requiredString,
-    bio: z.string().max(1000, "Must be at most 100 characters"),
+    bio: z.string().max(1000, "Must be at most 1000 characters"),
 });
 
 export type UpdateUserProfileValues = z.infer<typeof updateUserProfileSchema>;
 
-
-
-
+export const createCommentSchema = z.object({
+    content: requiredString,
+});
