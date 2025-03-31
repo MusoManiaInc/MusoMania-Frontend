b66a5a901605ad969e273368ee51193d9bf81301
miran <miranararat722@gmail.com>
2025-03-31 01:25:00 -0400

    make chat work

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,67 +1,79 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { createUploadthing, FileRouter } from "uploadthing/next-legacy"
-import { UploadThingError, UTApi } from "uploadthing/server"
+import streamServerClient from "@/lib/stream";
+import { createUploadthing, FileRouter } from "uploadthing/next";
+import { UploadThingError, UTApi } from "uploadthing/server";
 
 const f = createUploadthing();
 
 export const fileRouter = {
     avatar: f({
-        image: { maxFileSize: "512KB"}
-    }).middleware(async () => {
-        const {user} = await validateRequest();
-
-        if(!user) throw new UploadThingError("Unauthorized");
-        
-        return {user};
+        image: { maxFileSize: "512KB" },
     })
-    .onUploadComplete(async ({metadata, file}) => {
-        const oldAvatarUrl = metadata.user.avatarUrl;
+        .middleware(async () => {
+            const { user } = await validateRequest();
 
-        if (oldAvatarUrl) {
-            try {
-                const oldFileKey = new URL(oldAvatarUrl).pathname.split("/f/")[1];
-                
-                await new UTApi().deleteFiles(oldFileKey);
+            if (!user) throw new UploadThingError("Unauthorized");
 
-            } catch (error) {
-                console.error("Error extracting old avatar key:", error);
+            return { user };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            const oldAvatarUrl = metadata.user.avatarUrl;
+
+            if (oldAvatarUrl) {
+                const key = oldAvatarUrl.split(
+                    `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+                )[1];
+
+                await new UTApi().deleteFiles(key);
             }
-        }
 
-        const newAvatarUrl = `https://${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}.ufs.sh${new URL(file.url).pathname}`;
+            const newAvatarUrl = file.url.replace(
+                "/f/",
+                `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+            );
 
+            await Promise.all([
+                prisma.user.update({
+                    where: { id: metadata.user.id },
+                    data: {
+                        avatarUrl: newAvatarUrl,
+                    },
+                }),
+                streamServerClient.partialUpdateUser({
+                    id: metadata.user.id,
+                    set: {
+                        image: newAvatarUrl,
+                    },
+                }),
+            ]);
 
-        await prisma.user.update({
-            where: {id: metadata.user.id},
-            data: {
-                avatarUrl: newAvatarUrl,
-            },
-        });
-
-        return {avatarUrl: newAvatarUrl}
-    }),
+            return { avatarUrl: newAvatarUrl };
+        }),
     attachment: f({
-        image: {maxFileSize: "4MB", maxFileCount: 5},
-        video: {maxFileCount: 1}
+        image: { maxFileSize: "4MB", maxFileCount: 5 },
+        video: { maxFileSize: "64MB", maxFileCount: 5 },
     })
-    .middleware(async () =>{
-        const {user} = await validateRequest();
-        
-        if(!user) throw new UploadThingError("Unauthorized");
+        .middleware(async () => {
+            const { user } = await validateRequest();
 
-        return { }
-    })
-    .onUploadComplete(async ({file}) => {
-        const media = await prisma.media.create({
-            data: {
-                url: `https://${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}.ufs.sh${new URL(file.url).pathname}`,
-                type: file.type.startsWith("image") ? "IMAGE" : "VIDEO"
-            }
-        });
+            if (!user) throw new UploadThingError("Unauthorized");
 
-        return {mediaId: media.id};
-    })
+            return {};
+        })
+        .onUploadComplete(async ({ file }) => {
+            const media = await prisma.media.create({
+                data: {
+                    url: file.url.replace(
+                        "/f/",
+                        `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+                    ),
+                    type: file.type.startsWith("image") ? "IMAGE" : "VIDEO",
+                },
+            });
+
+            return { mediaId: media.id };
+        }),
 } satisfies FileRouter;
 
 export type AppFileRouter = typeof fileRouter;
